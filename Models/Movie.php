00191740ae04ae5ee93c9acfba6b611bd171c9c9
ludoff <ludovicofiore25@gmail.com>
2024-09-03 17:12:03 +0200

    feat: add first layout

--- Models/Movie.php
+++ Models/Movie.php
@@ -18,3 +18,9 @@ class Movie {
 // $movie1 = new Movie('Rocky I', 1976, ['Sylvester Stallone', 'Talia Shire', 'Burt Young']);
 // $movie2 = new Movie('Rocky II', 1979, ['Sylvester Stallone', 'Carl Weathers', 'Burgess Meredith']);
 // $movie3 = new Movie('Rocky III', 1982, ['Sylvester Stallone', 'Carl Weathers', 'Mr. T']);
+
+$movies = [
+    new Movie('Rocky I', 1976, ['Sylvester Stallone', 'Talia Shire', 'Burt Young']),
+    new Movie('Rocky II', 1979, ['Sylvester Stallone', 'Carl Weathers', 'Burgess Meredith']),
+    new Movie('Rocky III', 1982, ['Sylvester Stallone', 'Carl Weathers', 'Mr. T']),
+];
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 require_once __DIR__ . '/Models/Movie.php';
-require_once __DIR__ . '/data/db.php';
+// require_once __DIR__ . '/data/db.php';
 var_dump($movies);
 
 ?>
@@ -11,8 +11,26 @@ var_dump($movies);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>esercizio oop</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    
+    <div class="container">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">titolo film</h5>
+                <h6 class="card-subtitle mb-2 text-muted">anno</h6>
+                <p class="card-text">Attori:
+                    <ul>
+                        <li>attore</li>
+                    </ul>
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
