eb219b048a3b7f8e7031d4eef3ed2e99f3201202
ludoff <ludovicofiore25@gmail.com>
2024-09-03 16:07:40 +0200

    feat: add class with instances and attributes

---
+++ Models/Movie.php
@@ -0,0 +1,14 @@
+<?php
+
+class Movie {
+    // attributi
+    public $title;
+    public $year;
+    public $actors = [];
+
+}
+
+// istanze
+$movie1 = new Movie();
+$movie2 = new Movie();
+$movie3 = new Movie();
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+require_once __DIR__ . '/Models/Movie.php';
+
+var_dump($Movie);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>esercizio oop</title>
+</head>
+<body>
+    
+</body>
+</html>
